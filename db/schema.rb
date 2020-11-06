e529beac996ab36a4c01fde34ff34b7297deb41e
EC2 Default User <ec2-user@ip-172-31-45-107.ap-northeast-1.compute.internal>
2020-11-06 06:38:01 +0000

    [ADD] change each files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_06_030459) do
+ActiveRecord::Schema.define(version: 2020_11_06_055025) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2020_11_06_030459) do
     t.string "nickname"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "introduces"
+    t.string "user_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
