8395fca49e360cc57781fab5db346805ee2b9ced
EC2 Default User <ec2-user@ip-172-31-45-107.ap-northeast-1.compute.internal>
2022-02-21 01:07:16 +0000

    altered each files

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,13 +5,13 @@ class LikesController < ApplicationController
     @book = Book.find(params[:book_id])
     like = current_user.likes.new(book_id: @book.id)
     like.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     like = current_user.likes.find_by(book_id: @book.id)
     like.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 end
